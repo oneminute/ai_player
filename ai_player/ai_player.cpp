b0042e647c6c120bcc9ff9059931630c401eac53
oneminute <oneminute3b@gmail.com>
2025-02-12 20:17:18 -0600

    init the project

---
+++ ai_player/ai_player.cpp
@@ -0,0 +1,12 @@
+﻿// ai_player.cpp : Defines the entry point for the application.
+//
+
+#include "ai_player.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ ai_player/ai_player.h
@@ -0,0 +1,8 @@
+﻿// ai_player.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
